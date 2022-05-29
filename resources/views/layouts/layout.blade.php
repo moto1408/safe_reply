9a4d56a0716a2ae3e08e622a985939821bf60480
moto1408 <m.hayashi1408@gmailcom>
2022-05-29 11:50:27 +0900

    微調整

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -22,7 +22,7 @@
 
 <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" data-auto-replace-svg="nest"></script>
 </head>
-<body class="m-auto" style="width:850px">
+<body class="m-auto" style="width:400px">
 {{-- 共通ヘッダー --}}
 @include('layouts.parts.header')
 
